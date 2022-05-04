37a639e9fa3795e9602f9254d9e718ab8c2969f7
Chicco80 <chicco80lisa@gmail.com>
2022-05-04 17:45:51 +0200

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,97 @@
 <template>
-  <header>
-    
-  </header>
+   <header>
+   <div class="logo"><img src="../assets/img/dc-logo.png" alt="logo"></div>
+   <nav>
+     <ul>
+       <li v-for="(item, index) in links" :key="index" :class="{'selected' : item.active}">{{item.testo}}</li>
+     </ul>
+   </nav>
+ </header>
   
 </template>
 
 <script>
 export default {
   name: 'AppHeader',
+  data(){
+    return{
+      links:[
+        {
+          testo:'CHARACTERS',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'COMICS',
+          link:'',
+          active:'true',
+        },
+        {
+          testo:'MOVIES',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'TV',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'GAMES',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'COLLECTIBLES',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'VIDEO',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'FANS',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'NEWS',
+          link:'',
+          active:'false',
+        },
+        {
+          testo:'SHOP',
+          link:'',
+          active:'false',
+        },
+      ],
+    }
+  }
  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import '../style/variables';
+header{
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  
+}
 
+  ul{
+    display: flex;
+  }
+  li{
+    padding: 10px;
+    list-style-type: none;
+  }
+  .selected{
+    text-decoration: underline;
+  }
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="miodiv">
+       <p> --> Content goes here  </p> 
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppMain',
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
